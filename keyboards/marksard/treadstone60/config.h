4866eccc208682c15d3544cba48a8661948808bd
marksard <marksard@rocketmail.com>
2021-04-05 17:00:41 +0900

    treadstone60 enable rgb matrix

--- keyboards/marksard/treadstone60/config.h
+++ keyboards/marksard/treadstone60/config.h
@@ -57,25 +57,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BACKLIGHT_LEVELS 3
 
 #define RGB_DI_PIN D3
-#ifdef RGB_DI_PIN
-  #define RGBLED_NUM 6
+#ifdef RGBLIGHT_ENABLE
+// #ifdef RGB_DI_PIN
+  #define RGBLED_NUM 61
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 /*== all animations enable ==*/
-//   #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
-  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-  #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
-//   #define RGBLIGHT_EFFECT_ALTERNATING
+  // #define RGBLIGHT_EFFECT_BREATHING
+  // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  // #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  // #define RGBLIGHT_EFFECT_SNAKE
+  // #define RGBLIGHT_EFFECT_KNIGHT
+  // #define RGBLIGHT_EFFECT_CHRISTMAS
+  // #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  // #define RGBLIGHT_EFFECT_RGB_TEST
+  // #define RGBLIGHT_EFFECT_ALTERNATING
 /*== customize breathing effect ==*/
   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
@@ -84,58 +85,69 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
 #endif
 
+#ifdef RGB_MATRIX_ENABLE
+  #define RGBLED_NUM 61    // Number of LEDs
+  #define DRIVER_LED_TOTAL RGBLED_NUM
+  #define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
+  // #define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+  // #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+  #define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
+  // #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+  // #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+  #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150  // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+  #define RGB_MATRIX_HUE_STEP 8
+  #define RGB_MATRIX_SAT_STEP 8
+  #define RGB_MATRIX_VAL_STEP 8
+  #define RGB_MATRIX_SPD_STEP 10
+
+//   #define DISABLE_RGB_MATRIX_ALPHAS_MODS
+//   #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+//   #define DISABLE_RGB_MATRIX_BREATHING
+//   #define DISABLE_RGB_MATRIX_BAND_SAT
+//   #define DISABLE_RGB_MATRIX_BAND_VAL
+//   #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+//   #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+//   #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+//   #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+//   #define DISABLE_RGB_MATRIX_CYCLE_ALL
+//   #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+//   #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+//   #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+//   #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+//   #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+//   #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+//   #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+//   #define DISABLE_RGB_MATRIX_DUAL_BEACON
+//   #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+//   #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+//   #define DISABLE_RGB_MATRIX_RAINDROPS
+//   #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+
+  #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+//   #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+//   #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+
+//   #define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
+
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+//   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+
+//   #define DISABLE_RGB_MATRIX_SPLASH
+//   #define DISABLE_RGB_MATRIX_MULTISPLASH
+//   #define DISABLE_RGB_MATRIX_SOLID_SPLASH
+//   #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+  #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP
+#endif
+
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
- * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
- */
-// #define GRAVE_ESC_CTRL_OVERRIDE
-
-/*
- * Force NKRO
- *
- * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
- * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
- * makefile for this to work.)
- *
- * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
- * until the next keyboard reset.
- *
- * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
- * fully operational during normal computer usage.
- *
- * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
- * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
- * bootmagic, NKRO mode will always be enabled until it is toggled again during a
- * power-up.
- *
- */
-//#define FORCE_NKRO
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
 
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
--- keyboards/marksard/treadstone60/keymaps/via/rules.mk
+++ keyboards/marksard/treadstone60/keymaps/via/rules.mk
@@ -1,1 +1,4 @@
 VIA_ENABLE = yes
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
