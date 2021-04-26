a6bafa8c16ed9ef6cf814ceefa656a50ca34fbc9
marksard <marksard@rocketmail.com>
2021-04-26 13:49:39 +0900

    treadstone60 update via rules.mk

--- keyboards/marksard/treadstone60/keymaps/via/rules.mk
+++ keyboards/marksard/treadstone60/keymaps/via/rules.mk
@@ -1,4 +1,4 @@
 VIA_ENABLE = yes
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
