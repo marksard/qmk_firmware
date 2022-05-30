83d9b374403407837368f96c37a28215685660e7
marksard <marksard@rocketmail.com>
2022-05-30 17:32:07 +0900

    rebuild jj40 keymap

---
+++ keyboards/kprepublic/jj40/keymaps/marksard/keymap.c
@@ -0,0 +1,63 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#define LOWER FN_MO13
+#define RAISE FN_MO23
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[0] = LAYOUT_ortho_4x12(
+  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO, KC_NO, KC_Y, KC_U, KC_I, KC_O, KC_P, 
+  MT(MOD_LCTL,KC_A), KC_S, KC_D, KC_F, KC_G, KC_NO, KC_NO, KC_H, KC_J, KC_K, KC_L, MT(MOD_LCTL | MOD_RCTL,KC_ENT), 
+  MT(MOD_LSFT,KC_Z), KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_LSFT | MOD_RSFT,KC_SLSH), 
+  KC_LCTL, KC_LGUI, KC_LALT, MO(1), LT(1,KC_BSPC), LT(1,KC_BSPC), LT(1,KC_SPC), LT(1,KC_SPC), MO(2), KC_RGUI, KC_APP, KC_RCTL
+),
+[1] = LAYOUT_ortho_4x12(
+  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, 
+  MT(MOD_LCTL,KC_F6), KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SCLN, MT(MOD_LCTL | MOD_RCTL,KC_QUOT), 
+  MT(MOD_LSFT,KC_F11), KC_F12, KC_ESC, KC_TAB, A(KC_GRV), KC_DEL, KC_NO, KC_DEL, KC_NO, KC_TILD, KC_JYEN, MT(MOD_LSFT | MOD_RSFT,KC_RO), 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, MO(3), MO(3), MO(3), KC_TRNS, KC_TRNS, KC_TRNS
+),
+[2] = LAYOUT_ortho_4x12(
+  KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, 
+  KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LCTL, 
+  KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_COMM, KC_DOT, MT(MOD_LSFT | MOD_RSFT,KC_SLSH), 
+  KC_TRNS, KC_TRNS, KC_TRNS, MO(3), MO(3), MO(3), KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+),
+[3] = LAYOUT_ortho_4x12(
+  RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+  RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, 
+  RGB_MOD, RGB_HUI, RGB_SAD, RGB_VAD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+)
+
+};
+
+// Layer tap
+#define KC_BSLO  LT(1, KC_BSPC)
+#define KC_SPRA  LT(2, KC_SPC)
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_BSLO:
+      return TAPPING_LAYER_TERM;
+    case KC_SPRA:
+      return TAPPING_LAYER_TERM;
+    default:
+      return TAPPING_TERM;
+  }
+}
