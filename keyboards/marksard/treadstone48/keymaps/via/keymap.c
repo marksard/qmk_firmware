9f8ee8e185c2dfd97abe897dfd927c7838af7837
marksard <marksard@rocketmail.com>
2022-06-15 16:44:45 +0900

    [treadstone48] update via keymap

--- keyboards/marksard/treadstone48/keymaps/via/keymap.c
+++ keyboards/marksard/treadstone48/keymaps/via/keymap.c
@@ -14,12 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "../common/oled_helper.h"
-
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -33,50 +27,52 @@ enum layer_number {
 };
 
 enum custom_keycodes {
-  LOWER = SAFE_RANGE,
-  RAISE,
-  ADJUST,
-  KANJI,
+  KANJI = SAFE_RANGE,
   RGBRST
 };
-
-#define KC_MLAD  MO(_ADJUST)
-
-#define KC_ALAP  LALT_T(KC_APP)
-#define KC_JEQL  LSFT(KC_MINS)
-#define KC_ENSF  RSFT_T(KC_ENT)
-#define KC_ROSF  RSFT_T(KC_RO)
 
 // Layer tap
 #define KC_BSLO  LT(_LOWER, KC_BSPC)
 #define KC_SPRA  LT(_RAISE, KC_SPC)
+#define KC_MLAD  MO(_ADJUST)
+
+// Base layer mod tap
+#define KC_TBCT  LCTL_T(KC_TAB)
+#define KC_ENCT  RCTL_T(KC_ENT)
+#define KC_ROSF  RSFT_T(KC_RO)
+
+// Lower layer mod tap
+#define KC_QUCT  RCTL_T(KC_QUOT)
+
+// Other
+#define KC_ALAP  RALT_T(KC_APP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_base( \
   //,--------------------------------------------------------------------------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,          KC_ENSF,\
+      KC_TBCT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,          KC_ENCT,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_UP,         \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_LALT, KC_LGUI,   LOWER,          KC_BSLO,          KC_SPRA,   RAISE, KC_ALAP, KC_LEFT, KC_DOWN, KC_RGHT,\
+      KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,          KC_BSLO,          KC_SPRA, KC_MLAD, KC_ALAP, KC_LEFT, KC_DOWN, KC_RGHT,\
   //`-------------------------------------------------------------------------------------------------------------------'
-       KC_ROSF \
+      KC_ROSF \
   // ExtraKey: This key is an extra key. REV1 is a split back space. REV2 is to the right of the arrow-up key.
   ),
 
   [_LOWER] = LAYOUT_base( \
   //,--------------------------------------------------------------------------------------------------------------------.
-       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_MINS,  KC_EQL, KC_JYEN, KC_LBRC, KC_RBRC,           KC_DEL,\
+       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_MINS,  KC_EQL, KC_JYEN, KC_LBRC, KC_RBRC,          KC_BSLS,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
-      _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT, KC_BSLS,          _______,\
+      _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_QUCT,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-      _______,  KC_F11,  KC_F12, XXXXXXX,   KANJI,   KANJI,  KC_DEL, XXXXXXX, XXXXXXX,   KC_RO,  KC_GRV, KC_PGUP,         \
+      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX,   KANJI,  KC_DEL, XXXXXXX, XXXXXXX,   KC_RO,  KC_GRV, KC_PGUP,         \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______,          _______,          KC_MLAD, _______, XXXXXXX, KC_HOME, KC_PGDN,  KC_END,\
   //`-------------------------------------------------------------------------------------------------------------------'
-    XXXXXXX \
+      _______ \
   // ExtraKey: This key is an extra key. REV1 is a split back space. REV2 is to the right of the arrow-up key.
   ),
 
@@ -88,49 +84,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_BSLS, XXXXXXX,         \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______,          _______,          _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      _______, _______, _______, _______,          KC_MLAD,          _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //`-------------------------------------------------------------------------------------------------------------------'
-    XXXXXXX \
+      _______ \
   // ExtraKey: This key is an extra key. REV1 is a split back space. REV2 is to the right of the arrow-up key.
   ),
 
   [_ADJUST] = LAYOUT_base( \
   //,--------------------------------------------------------------------------------------------------------------------.
-      XXXXXXX,   RESET,  RGBRST, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS,          KC_PSCR,\
+      XXXXXXX, QK_BOOT,  RGBRST, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS,          KC_PSCR,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
-      XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,          KC_NLCK,\
+      KC_CAPS, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX,          KC_NLCK,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-      _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_U,         \
+      _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_MS_U,         \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______,          XXXXXXX,          XXXXXXX, _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R,\
   //`-------------------------------------------------------------------------------------------------------------------'
-    XXXXXXX \
+      _______ \
   // ExtraKey: This key is an extra key. REV1 is a split back space. REV2 is to the right of the arrow-up key.
   )
 };
 
 #define L_BASE _BASE
-#define L_LOWER (1<<_LOWER)
-#define L_RAISE (1<<_RAISE)
-#define L_ADJUST (1<<_ADJUST)
-#define L_ADJUST_TRI (L_ADJUST|L_RAISE|L_LOWER)
+#define L_LOWER ((layer_state_t)1<<_LOWER)
+#define L_RAISE ((layer_state_t)1<<_RAISE)
+#define L_ADJUST ((layer_state_t)1<<_ADJUST)
+#define L_ADJUST_1 (L_ADJUST|L_LOWER)
+#define L_ADJUST_2 (L_ADJUST|L_RAISE)
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 #include <stdio.h>
 #include <string.h>
+#include "../common/oled_helper.h"
 
 typedef struct {
   uint8_t state;
   char name[8];
 }LAYER_DISPLAY_NAME;
 
-#define LAYER_DISPLAY_MAX 5
+#define LAYER_DISPLAY_MAX 6
 const LAYER_DISPLAY_NAME layer_display_name[LAYER_DISPLAY_MAX] = {
   {L_BASE, "Base"},
-  {L_BASE + 1, "Base"},
   {L_LOWER, "Lower"},
   {L_RAISE, "Raise"},
-  {L_ADJUST_TRI, "Adjust"}
+  {L_ADJUST, "Adjust"},
+  {L_ADJUST_1, "Adjust"},
+  {L_ADJUST_2, "Adjust"}
 };
 
 static inline const char* get_leyer_status(void) {
@@ -139,7 +138,7 @@ static inline const char* get_leyer_status(void) {
     if (layer_state == 0 && layer_display_name[i].state == default_layer_state) {
 
       return layer_display_name[i].name;
-    } else if (layer_state != 0 && layer_display_name[i].state == layer_state) {
+    } else if (layer_state != 0 && (layer_display_name[i].state == layer_state)) {
 
       return layer_display_name[i].name;
     }
@@ -160,12 +159,11 @@ static inline void render_keymap_status(void) {
   oled_write(layer_status_buf, false);
 }
 
-#define UPDATE_KEYMAP_STATUS() update_keymap_status()
-
 static inline void render_status(void) {
 
   UPDATE_LED_STATUS();
   RENDER_LED_STATUS();
+  update_keymap_status();
   render_keymap_status();
   UPDATE_LOCK_STATUS();
   RENDER_LOCK_STATUS();
@@ -179,18 +177,15 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
 
   if (is_keyboard_master()) {
     render_status();
   } else {
     render_logo();
   }
+  return false;
 }
-
-#else
-
-#define UPDATE_KEYMAP_STATUS()
 
 #endif
 
@@ -205,25 +200,12 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-static inline void update_change_layer(bool pressed, uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-
-  pressed ? layer_on(layer1) : layer_off(layer1);
-  IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2) ? layer_on(layer3) : layer_off(layer3);
-}
-
-int RGB_current_mode;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   UPDATE_KEY_STATUS(keycode, record);
 
   bool result = false;
   switch (keycode) {
-    case LOWER:
-      update_change_layer(record->event.pressed, _LOWER, _RAISE, _ADJUST);
-      break;
-    case RAISE:
-      update_change_layer(record->event.pressed, _RAISE, _LOWER, _ADJUST);
-        break;
     case KANJI:
       if (record->event.pressed) {
         if (keymap_config.swap_lalt_lgui) {
@@ -247,6 +229,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
 
-  UPDATE_KEYMAP_STATUS();
   return result;
 }
